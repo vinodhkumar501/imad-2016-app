d0d25b70c42cd171411ad773305af0b76787064f
vinodhkumar501 <vinodhkumar501@gmail.com>
2016-11-07 20:27:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var bodyParser=require('body-parser');
 var session=require('express-session');
 
 var config={
-    user:'ajasharma93',
-    database: 'ajasharma93',
+    user:'vinodhkumar501',
+    database: 'vinodhkumar501',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
